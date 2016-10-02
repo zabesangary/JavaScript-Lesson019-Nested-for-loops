e9dfa2021a402457c1b8b9211b26402da8ca6d63
Zabe <zabivirdee@gmail.com>
2016-10-02 14:54:54 +0200

    Nest for loop

---
+++ script.js
@@ -0,0 +1,11 @@
+var students = ['Forogoh', 'Sohrab', 'Elham'];
+var teachers = [ 'Zabe', 'Javeed'];
+var totalTeachers = [];
+var result = 0;
+   for (var i = 0; i < students.length; i++) {
+      for (var j = 0; j < teachers.length; j++) {
+         totalTeachers[result] = students[i] + teachers[j];
+          console.table("total length is");
+result++
+}
+}
